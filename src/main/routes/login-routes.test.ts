71dfba5d89f9fc8c4d027e1da8764cb37731876e
Ismael de Jesus <mael.dazareia@gmail.com>
2021-03-21 16:55:40 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login with invalid credentials', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_email@email.com',
+          password: 'any_password'
+        })
+        .expect(401)
+    })
   })
 })
