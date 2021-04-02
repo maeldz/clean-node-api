ab5fff04802a33141c6ca37dc2173490b5131d1d
Ismael de Jesus <mael.dazareia@gmail.com>
2021-04-01 21:08:31 -0300

    chore: update jest config for ignore protocol files from coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,10 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/**/protocols/*.ts'
+    
   ],
   coverageDirectory: 'coverage',
   transform: {
