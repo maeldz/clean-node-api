8c92188b4c5650c0e54dbfd191b027f9b8fe1e64
Ismael de Jesus <mael.dazareia@gmail.com>
2021-04-22 07:37:43 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -7,6 +7,7 @@ export const surveyResultPath = {
     ],
     tags: ['survey'],
     summary: 'Api para criar a resposta de uma enquete',
+    description: 'Essa rota só pode ser acessada por **usuários autenticados**',
     parameters: [{
       in: 'path',
       name: 'surveyId',
@@ -26,6 +27,46 @@ export const surveyResultPath = {
         }
       }
     },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+  get: {
+    security: [
+      {
+        apiKeyAuth: []
+      }
+    ],
+    tags: ['survey'],
+    summary: 'Api para consultar o resultado de uma enquete',
+    description: 'Essa rota só pode ser acessada por **usuários autenticados**',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      description: 'ID da enquete a ser consultada',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
     responses: {
       200: {
         description: 'Sucesso',
